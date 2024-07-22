88fe413387987770ca8cd6661c2601bb218ef677
NarciSource <jwch11@gmail.com>
2024-07-22 12:57:12 +0900

    feat: 특정 캐릭터 초기 조건 생성

--- Board.js
+++ Board.js
@@ -6,6 +6,8 @@ export default class Board {
         .fill(null)
         .map(() => Array(COLUMN_SIZE).fill(null));
 
+    #characters;
+
     // please remove this
     board() {
         return this.#board;
@@ -14,6 +16,19 @@ export default class Board {
     score() {
         const value_sum = this.#board.reduce((acc, columns) => acc + columns.filter((i) => i).reduce((acc, cur) => acc + cur, 0), 0);
         return value_sum;
+    }
+
+    set(position, character) {
+        const { column, row } = position;
+
+        if (row<0 || column<0 || row>=ROW_SIZE || column >=COLUMN_SIZE) {
+            throw "보드 범위를 벗어나 있습니다."
+        }
+        if (this.#board[row][column]) {
+            throw "해당 위치에 다른 말이 있습니다.";
+        }
+
+        this.#board[row][column] = character;
     }
 
     display(line_num) {
