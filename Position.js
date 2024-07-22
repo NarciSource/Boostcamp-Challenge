64a0c158bbf25c60e30bc075698852e882d18e9c
NarciSource <jwch11@gmail.com>
2024-07-22 13:13:14 +0900

    feat: Character, Position 클래스 생성

---
+++ Position.js
@@ -0,0 +1,20 @@
+export class Row {
+    static A = 0;
+    static B = 1;
+    static C = 2;
+    static D = 3;
+    static E = 4;
+}
+export class Column {
+    static 1 = 0;
+    static 2 = 1;
+    static 3 = 2;
+    static 4 = 3;
+    static 5 = 4;
+    static 6 = 5;
+}
+
+export default class Position {
+    row;
+    column;
+}
