0af0668e89a2887f5b2dce340243aa6e7257148e
NarciSource <jwch11@gmail.com>
2024-07-23 16:14:32 +0900

    refactor: Path 파일로 이동

--- path.js
+++ path.js
@@ -1,36 +1,20 @@
-import { parse_filepath } from "./parse_filepath.js";
+const path_regex = /^(\/|\w:\\)?([\w\/\.\\]+(?:\/|\\))?([^./]+)(\.\w+)?$/;
 
 export default class Path {
     root;
-    base;
     name;
     ext;
     components;
-    #absolute_string;
-    #exist_file;
-    #file_size;
 
     constructor(src) {
-        const {
-            root,
-            base,
-            name,
-            ext,
-            components,
-            absolute_string,
-            exist_file,
-            file_size,
-        } = parse_filepath(src);
+        const { root, name, ext, components } = this.#parse_filepath(src);
 
         this.root = root;
-        this.base = base;
         this.name = name;
         this.ext = ext;
         this.components = components;
-        this.#absolute_string = absolute_string;
-        this.#exist_file = exist_file;
-        this.#file_size = file_size;
     }
+
     stringify() {
         JSON.stringify({
             root: this.root,
@@ -38,9 +22,9 @@ export default class Path {
             name: this.name,
             ext: this.ext,
             components: this.components,
-            absolute_string: this.#absolute_string,
-            exist_file: this.#exist_file,
-            file_size: this.#file_size,
+            absolute_string: this.absolute_string(),
+            exist_file: this.exist_file(),
+            file_size: this.file_size(),
         });
     }
 
@@ -49,5 +33,58 @@ export default class Path {
     }
     deleteLast_component() {
         this.components.pop();
+    }
+
+    #get_absolute(components = ["."]) {
+        const current_dir = process.cwd().split(/\/|\\/);
+
+        if (process.platform === "linux") {
+            current_dir[0] = "/";
+        }
+
+        let index;
+        for (const [i, component] of Object.entries(components)) {
+            index = i;
+            if (component === "..") {
+                current_dir.pop();
+            } else if (component === ".") {
+                index = i + 1;
+                break;
+            } else {
+                break;
+            }
+        }
+        return current_dir.concat(components.slice(index));
+    }
+
+    #parse_filepath(path_string) {
+        if (path_regex.test(path_string)) {
+            let [, root, components_string, name, ext] =
+                path_regex.exec(path_string);
+
+            let components = components_string
+                ?.split(/\/|\\/)
+                .filter((i) => i.length > 0);
+
+            if (!root) {
+                components = this.#get_absolute(components);
+                root = components[0];
+            } else {
+                root =
+                    process.platform === "linux"
+                        ? "/"
+                        : process.cwd().split(/\/|\\/)[0];
+                components = [root].concat(components || []);
+            }
+
+            return {
+                root,
+                name,
+                ext,
+                components,
+            };
+        } else {
+            throw "경로에 오류가 있습니다.";
+        }
     }
 }
