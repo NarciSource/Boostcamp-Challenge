4b83660ec6aac951c2e5aeef7d9ab7640aae5a15
NarciSource <jwch11@gmail.com>
2024-07-29 13:48:25 +0900

    feat: 주기적으로 대기큐 확인

--- Manager.js
+++ Manager.js
@@ -2,8 +2,19 @@ class Manager {
     ready_queue = [];
     delivery_queue = [];
 
+    constructor() {
+        setInterval(this.ready_queue_watcher.bind(this), 1000);
+    }
+
     reception(parcels) {
-        this.ready_queue.concat(parcels);
+        this.ready_queue = [...this.ready_queue, ...parcels];
+    }
+
+    async ready_queue_watcher() {
+        if (this.ready_queue.length) {
+            console.log("watch");
+            this.ready_queue.shift();
+        }
     }
 }
 
