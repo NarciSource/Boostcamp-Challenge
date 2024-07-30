0a2429df87213e09d7b7a874eee2bf15e2545cb3
NarciSource <jwch11@gmail.com>
2024-07-30 16:51:01 +0900

    fix: 입력 형식 에러 체크

--- main.ts
+++ main.ts
@@ -13,22 +13,38 @@ const get_input = (prompt: string): Promise<string> =>
 (async function main() {
     const pos = new POS();
 
-    const input = await get_input("작업자와 배달 기사 인원 수를 지정하세요(띄어서 입력). ");
-    const [n, m] = input.split(" ");
-    console.log("분류작업자: ", n, "배달 기사", m);
+    // input numbers of workers
+    let n: number, m: number;
+    while (true) {
+        try {
+            const input = await get_input("작업자와 배달 기사 인원 수를 지정하세요(띄어서 입력). ");
 
+            const regex = /^(\d+) (\d+)$/;
+            [n, m] = (([, n, m]) => [parseInt(n), parseInt(m)])(regex.exec(input));
+
+            console.log("분류작업자: ", n, "배달 기사", m);
+            break;
+        } catch (e) {
+            console.error(e, "올바르지 않은 입력입니다.");
+        }
+    }
+
+    // hire workers
     for (const center of manager.logistics_centers) {
-        const classify_newcomers = Array(parseInt(n))
+        const classify_newcomers = Array(n)
             .fill(null)
             .map(() => new ClassifyWorker());
-        const delivery_newcomers = Array(parseInt(m))
+        const delivery_newcomers = Array(m)
             .fill(null)
             .map(() => new DeliveryWorker());
 
         center.hire([...classify_newcomers, ...delivery_newcomers]);
     }
+
+    // regular output
     dashBoard.display();
 
+    // input parcels
     while (true) {
         try {
             if (pos.input(await get_input("> ")) === "exit") {
