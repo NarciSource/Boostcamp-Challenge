e7a8755bb7eb3b35ffa7adabb4c55e463c8683dc
cozym <ddongyee99@daum.net>
2024-07-31 16:29:44 +0900

    Feat: init() 구현

--- main.ts
+++ main.ts
@@ -1,16 +1,18 @@
-// init 디렉토리명
-// add 디렉토리명
-// status 디렉토리명
-// commit 디렉토리명
-// log 디렉토리명
-// restore 디렉토리명 {8자리 | 64자리 커밋해시값}
-// https://www.npmjs.com/package/commander
+import fs from "fs";
 
-const [command, directoryPath, hashValue] = process.argv;
+const [, , command, directoryPath, hashValue] = process.argv;
 
+/**
+ * init 디렉토리명
+ * add 디렉토리명
+ * status 디렉토리명
+ * commit 디렉토리명
+ * log 디렉토리명
+ * restore 디렉토리명 {8자리 | 64자리 커밋해시값}
+ */
 switch (command) {
     case "init":
-        //init();
+        init(directoryPath);
         break;
     case "add":
         break;
@@ -22,4 +24,18 @@ switch (command) {
         break;
     default:
         break;
+}
+
+/**
+ * 디렉토리명/.mit/objects/
+ * 디렉토리명/.mit/index/
+ * https://www.geeksforgeeks.org/node-js-fs-mkdir-method/
+ */
+function init(directoryPath) {
+    fs.mkdir(`${directoryPath}/.mit/objects`, { recursive: true }, (error) => {
+        console.log(error);
+    });
+    fs.mkdir(`${directoryPath}/.mit/index`, { recursive: true }, (error) => {
+        console.log(error);
+    });
 }
