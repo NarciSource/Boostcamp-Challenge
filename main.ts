2ec96c15c0966de473aa8ea9aef37e43bb5049d6
NarciSource <jwch11@gmail.com>
2024-07-31 22:33:29 +0900

    feat: staging hash 값 비교

--- main.ts
+++ main.ts
@@ -40,9 +40,7 @@ function init(directoryPath) {
     fs.mkdir(`${directoryPath}/.mit/objects`, { recursive: true }, (error) => {
         console.log(error);
     });
-    fs.mkdir(`${directoryPath}/.mit/index`, { recursive: true }, (error) => {
-        console.log(error);
-    });
+    fs.writeFileSync(`${directoryPath}/.mit/index`, "");
 }
 
 /**
@@ -52,14 +50,21 @@ function init(directoryPath) {
 async function add(directoryPath) {
     const files = await glob(`${directoryPath}/**/*`, { ignore: ["node_modules/**", ".mit/**"] });
 
-    for (const filePath of files) {
-        console.log(hashObject(filePath, directoryPath));
+    const curStaging = files.map((filePath) => hashObject(filePath, directoryPath));
+
+    const hash = crypto.createHash("sha256");
+    const curStagingHash = hash.update(curStaging.join(" ")).digest("hex");
+
+    const preStagingHash = fs.readFileSync(`${directoryPath}/.mit/index`, "utf8");
+
+    if (curStagingHash !== preStagingHash) {
+        fs.writeFileSync(`${directoryPath}/.mit/index`, curStagingHash);
     }
 }
 
 /**
  * 파일을 순회하며 blob객체 생성
- * blob 내용 zlip 압축
+ * blob 내용 zlib 압축
  * blob 객체 해싱 후 해시값 반환
  * https://nodejs.org/api/crypto.html
  * https://nodejs.org/api/zlib.html
@@ -89,3 +94,22 @@ function hashObject(filePath, directoryPath): string {
 
     return hashCode;
 }
+
+// 인덱스 파일의 바이너리 구조는 다음과 같습니다:
+
+//     헤더 (48바이트):
+//         Signature (4바이트): DIRC
+//         Version (4바이트): 버전 정보 (일반적으로 2가 사용됨)
+//         Checksum (4바이트): 헤더와 데이터에 대한 체크섬
+
+//     인덱스 항목 (변동 크기):
+//         File Mode (2바이트)
+//         Object Name (20바이트, SHA-1 해시)
+//         Stage (1바이트)
+//         Timestamp (4바이트, UNIX 타임스탬프)
+//         Size (4바이트)
+//         Path Length (2바이트)
+//         Path (변동 크기, NUL로 구분된 문자열)
+
+//     체크섬 (20바이트):
+//         Checksum: 인덱스 파일 전체에 대한 체크섬
