2491e52afd96aced4f40d0d5a224bba1170ab401
NarciSource <jwch11@gmail.com>
2024-08-05 22:02:03 +0900

    fix: 정규식 수정

--- parser.restore.ts
+++ parser.restore.ts
@@ -1,8 +1,8 @@
 import { Tuple } from "./parser.type";
 
 export default function parse([column_line, value_line]: [string, string]): Tuple {
-    const column_regex = /Column:\s*(\w+)/;
-    const value_regex = /Value:\s*(\d+)|"?(\w+)"?/;
+    const column_regex = /Column:\s*(\w+)/i;
+    const value_regex = /Value:\s*(?:(\d+)|"?(\w+)"?)/i;
 
     const [, name] = column_regex.exec(column_line);
     const [, number, str] = value_regex.exec(value_line);
