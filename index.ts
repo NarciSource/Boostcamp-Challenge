e611df32b09e5ea2c4b15456fe9d15fd45d761b6
NarciSource <jwch11@gmail.com>
2024-08-07 15:06:03 +0900

    feat: 클라이언트 IP, Port 로그

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import net from "node:net";
-//https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
+
 const server = net.createServer(function (client) {
-  console.log("Client connected");
+  const { remoteAddress, remotePort } = client;
+
+  console.log("Client connected", remoteAddress, remotePort);
+
   let buffer = "";
   const encoder = new TextEncoder();
 
@@ -9,13 +12,14 @@ const server = net.createServer(function (client) {
     const message = data.toString();
     const view = encoder.encode(message);
     buffer += message;
-    // window 줄넘김 = \r\n
 
+    // window 줄넘김 = \r\n
     if ((message == "\r\n" || message == "\n") && view.length <= 1024 && buffer.length >= 4) {
       client.write(buffer);
       buffer = "";
     }
   });
+
   client.on("end", function () {
     console.log("Client disconnected");
   });
