cbee75cc30b688d7e570d21498f43e400e5bd2ed
NarciSource <jwch11@gmail.com>
2024-08-07 15:29:01 +0900

    feat: 브로드캐스팅

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import net from "node:net";
 
+const clients = [];
+
 const server = net.createServer(function (client) {
+  clients.push(client);
+
   const { remoteAddress, remotePort } = client;
 
   console.log("Client connected", remoteAddress, remotePort);
+  client.write("Client connected " + remotePort);
 
   let buffer = "";
   const encoder = new TextEncoder();
@@ -15,6 +20,9 @@ const server = net.createServer(function (client) {
 
     // windows 줄넘김 = \r\n
     if ((message == "\r\n" || message == "\n") && view.length <= 1024 && buffer.length >= 4) {
+      for (const client of clients) {
+        client.write(buffer);
+      }
       client.write(buffer);
       buffer = "";
     }
