3f94a0e4c5f0a8ac2798a856ce5805ff39785f0a
HongBoogie <ghost1434@naver.com>
2024-08-07 15:00:44 +0900

    feat: 클라이언트 입력 크기 제한

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
-import net from "node:net"
-
+import net from "node:net";
+//https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
 const server = net.createServer(function (client) {
   console.log("Client connected");
+  let buffer = "";
+  const encoder = new TextEncoder();
+
   client.on("data", function (data) {
-    console.log("Client sent " + data.toString());
+    const message = data.toString();
+    const view = encoder.encode(message);
+    buffer += message;
+    // window 줄넘김 = \r\n
+
+    if ((message == "\r\n" || message == "\n") && view.length <= 1024 && buffer.length >= 4) {
+      client.write(buffer);
+      buffer = "";
+    }
   });
   client.on("end", function () {
     console.log("Client disconnected");
   });
-  client.write("Hello");
 });
 
 server.listen(2024, function () {
