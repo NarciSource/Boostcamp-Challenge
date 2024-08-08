6cc23a90d5111206be275d934f55e01df4f64894
NarciSource <jwch11@gmail.com>
2024-08-08 23:56:50 +0900

    fix: 클라이언트 에러 출력

--- client.getMessage.ts
+++ client.getMessage.ts
@@ -8,21 +8,25 @@ export default function getMessage(data: Buffer) {
     const { header, body }: Response = JSON.parse(data.toString());
     const message: string | NestedMessage = body?.data;
 
-    if (typeof message !== "string") {
-        if (message?.extra === "checkin") {
-            checkedInTime = header.time;
-        } else if (message?.extra === "checkout") {
-            const coreTime = header.time - checkedInTime;
+    if (header.code === 200) {
+        if (typeof message !== "string") {
+            if (message?.extra === "checkin") {
+                checkedInTime = header.time;
+            } else if (message?.extra === "checkout") {
+                const coreTime = header.time - checkedInTime;
 
-            const differenceInSeconds = Math.floor(coreTime / 1000);
-            const differenceInMinutes = Math.floor(differenceInSeconds / 60);
+                const differenceInSeconds = Math.floor(coreTime / 1000);
+                const differenceInMinutes = Math.floor(differenceInSeconds / 60);
 
-            console.log(`Core time = ${differenceInMinutes}min ${differenceInSeconds}sec`);
+                console.log(`Core time = ${differenceInMinutes}min ${differenceInSeconds}sec`);
 
-            client.end();
+                client.end();
+            }
+            console.log(message.message);
+        } else {
+            console.log(message);
         }
-        console.log(message.message);
     } else {
-        console.log(message);
+        console.log(header.code, header.errorMessage);
     }
 }
